c4b3fc316beea852115808417e3f6058f355d82c
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-02-12 12:03:30 +0600

    js

---
+++ Javascript.js
@@ -0,0 +1,47 @@
+/*
+01. What IS JS ?
+
+02. How does js work ?
+
+03. What is Event Loop:
+It is a core concept in JavaScript and 
+is used in other programming languages as well
+The event loop is a design pattern in computer programming that allows a 
+program to handle multiple events or tasks asynchronously and perform long-running 
+tasks without freezing the user interface. 
+
+04. how does js code executed browser?
+
+05. == vs === 
+
+06. what is the callback function ?
+A callback function is a function that is passed as an argument to another 
+function and is executed after some kind of event
+Callbacks are commonly used to handle asynchronously executing functions.
+
+07. Return Function 
+A return statement is used to return a value from a function 
+
+08. bind call apply ?
+
+09. Closure ?
+closure is combination of function he give to to other function from inner function 
+
+10.This keyword 
+The this keyword in JavaScript refers to the object 
+that is calling the function in which this is used
+
+11.what is event bubbling
+12.Hoisting   
+13.Recursive function 
+14. undefined vs null
+15.type of js 
+16.DOM
+17.
+
+
+
+
+
+
+*/
